3a4678771a4e43fa28135853e1f69a45b4b8078e
Scott Reis <scott.reis@vertive.com>
2013-02-24 17:36:06 -0600

    step through maze with spacebar

--- lib/maze/window.rb
+++ lib/maze/window.rb
@@ -2,15 +2,23 @@ module Maze
   class Window < ::Gosu::Window
 
     def initialize
-      super(640, 480, false, 100)
+      super(640, 480, false)
       self.caption = 'Maze'
-      @counter = 1
+      @counter, @but_down = 1, false
+      puts $walls[0]
+    end
+
+    def update
+      if button_down?(::Gosu::KbSpace) && !@but_down
+        @but_down = true
+        @counter += 1
+        puts $walls[@counter - 1] if $walls[@counter - 1]
+      end
+      @but_down = false if !button_down?(::Gosu::KbSpace)
     end
 
     def draw
-      puts $walls[@counter - 1] if $walls[@counter - 1]
       $walls.first(@counter).each(&:render)
-      @counter += 1
     end
 
   end
