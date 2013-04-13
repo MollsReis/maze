ffaae398fb554978dfd286a7cac2264b9babb206
EvilScott <reis.robert.s@gmail.com>
2013-04-12 20:07:22 -0500

    get LOS working with robots

--- lib/maze/entity/robot.rb
+++ lib/maze/entity/robot.rb
@@ -5,7 +5,7 @@ module Maze
 
       Z_INDEX = 2
       SPEED = 1.2
-      LOS_RANGE = 100
+      LOS_RANGE = 200
       BOUNDING_RADIUS = 10
       COLOR = Gosu::Color.new(0xFFCCCCCC)
 
@@ -30,20 +30,40 @@ module Maze
       end
 
       def move!
-        angle = Math.atan2(@y - @window.hero.y, @x - @window.hero.x)
-        x_move = Math.cos(angle) * SPEED
-        y_move = Math.sin(angle) * SPEED
+        x_move = Math.cos(angle_to_hero) * SPEED
+        y_move = Math.sin(angle_to_hero) * SPEED
         @x -= x_move unless wall_collide?(@x - x_move)
         @y -= y_move unless wall_collide?(nil, @y - y_move)
       end
 
       def see_hero?
         return false if distance_to_hero > LOS_RANGE
+        if @x.to_i < @window.hero.x.to_i
+          los_x_range = (@x.to_i..@window.hero.x.to_i).to_a
+        else
+          los_x_range = (@window.hero.x.to_i..@x.to_i).to_a
+        end
+        if @y.to_i < @window.hero.y.to_i
+          los_y_range = (@y.to_i..@window.hero.y.to_i).to_a
+        else
+          los_y_range = (@window.hero.y.to_i..@y.to_i).to_a
+        end
+        @window.walls.each do |wall|
+          wall.bounding_boxes.each do |box|
+            next if (box.horiz_range.to_a & los_x_range).empty?
+            next if (box.vert_range.to_a & los_y_range).empty?
+            return false
+          end
+        end
         true
       end
 
       def distance_to_hero
         Math.hypot(@x - @window.hero.x, @y - @window.hero.y)
+      end
+
+      def angle_to_hero
+        Math.atan2(@y - @window.hero.y, @x - @window.hero.x)
       end
 
       def bounding_box
